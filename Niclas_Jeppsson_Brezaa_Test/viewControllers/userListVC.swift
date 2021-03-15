5ed313e18b85937ca44c1e7f17383274b68316a8
Niclas Jeppsson <niclasjeppsson@gmail.com>
2021-03-15 17:02:33 +0000

    initial setup

--- Niclas_Jeppsson_Brezaa_Test/viewControllers/userListVC.swift
+++ Niclas_Jeppsson_Brezaa_Test/viewControllers/userListVC.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class userListVC: UIViewController {
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationControllerSetup()
+        
+    }
+    
+    private func navigationControllerSetup(){
+        
+        
     }
 
 
